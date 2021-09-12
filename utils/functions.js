16e7e22a64d3879a6728a9466adc07561160cc1c
Tomato6966 <68145571+Tomato6966@users.noreply.github.com>
2021-09-12 13:00:39 +0200

    Add files via upload

--- utils/functions.js
+++ utils/functions.js
@@ -1,18 +1,34 @@
+const Discord = require("discord.js");
 function escapeRegex(str){
     return str.replace(/[.*+?^${}()|[\]\\]/g, `\\$&`);
 }
 function generateHelpEmbed(guild){
-    return new Discord.MessageEmbed()
-    .setColor("BLURPLE")
-    .setTitle(`My very First Embed`)
-    .setDescription(`Hello I am ${client.user.username} and i am a cool bot!\n\n**These are my Commands:**`)
-    .setThumbnail(client.user.displayAvatarURL())
-    .addFields([
-        {name: "**ping**", value: `> *Shows the Ping of me.*`, inline: true},
-        {name: "**help**", value: `> *Gives you help!*`, inline: true},
-        {name: "**deploy**", value: `> *Adds the Slash Commands to this Server*`, inline: true},
-    ])
-    .setFooter(guild.name, guild.iconURL({dynamic: true}));
+    let embeds = []
+    embeds.push(new Discord.MessageEmbed()
+        .setColor("BLURPLE")
+        .setTitle(`This is my Help Information!`)
+        .setDescription(`Hello I am ${guild.me.user.username} and i am a cool bot!\n\n**These are my Commands:**`)
+        .setThumbnail(guild.me.user.displayAvatarURL())
+        .setFooter(guild.name, guild.iconURL({dynamic: true})))
+    embeds.push(new Discord.MessageEmbed()
+        .setColor("GREEN")
+        .setTitle(`💯 My Utility Commands!`)
+        .setThumbnail(guild.me.user.displayAvatarURL())
+        .addFields([
+            {name: "**ping**", value: `> *Shows the Ping of me.*`, inline: true},
+            {name: "**help**", value: `> *Gives you help!*`, inline: true},
+        ])
+        .setFooter(guild.name, guild.iconURL({dynamic: true})))
+    embeds.push(new Discord.MessageEmbed()
+        .setColor("RED")
+        .setTitle(`🚫 The Administration Commands!`)
+        .setThumbnail(guild.me.user.displayAvatarURL())
+        .addFields([
+            {name: "**deploy**", value: `> *Adds the Slash Commands to this Server*`, inline: true},
+            {name: "**prefix**", value: `> *Changes the Prefix of your Server*`, inline: true},
+        ])
+        .setFooter(guild.name, guild.iconURL({dynamic: true})))
+    return embeds;
 }
 
 module.exports.escapeRegex = escapeRegex;
